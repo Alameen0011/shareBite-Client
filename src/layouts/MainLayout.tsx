d4727526dc99498afa91647c6541800ab39f14ca
Al Ameen <sayedalameen1981@gmail.com>
2025-04-09 11:35:56 +0530

    small layout change

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -6,7 +6,7 @@ const MainLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex-1 container mx-auto p-4 pt-16 ">
+      <main className="flex-1 container mx-auto p-4 pt-16 min-h-screen">
         <Outlet />
       </main>
       <Footer />
